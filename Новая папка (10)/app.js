2320a9d09549e7aac6422c498a4be376de4e3222
Mary-Mei <109496345+Mary-Mei@users.noreply.github.com>
2022-11-18 19:20:58 +1000

    Add files via upload

---
+++ Новая папка (10)/app.js
@@ -0,0 +1,29 @@
+function getPicture() {
+    const picture = {
+        id: 3,
+        url: './picture/3',
+        description: '3/10',
+        comments: [
+            {
+                id: 3,
+                message: 'top',
+                user: {
+                    avatar: './picture/3.avatar',
+                    name: 'user333',
+                }
+            }
+        ]
+    };
+
+    return picture;
+}
+
+function getPictures() {
+    return [
+        getPicture(),
+        getPicture(),
+        getPicture(),
+    ];
+}
+
+console.log(getPictures()) 
